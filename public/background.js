d10e001cacaa5000f0bff9d800e800d094d09923
Supraja-Koppisetty <kvsupraja2710@gmail.com>
2025-09-27 22:48:40 -0400

    Added ability to save recorded events

--- public/background.js
+++ public/background.js
@@ -1,6 +1,16 @@
+let recordedClicks = [];
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if (msg.type === "RECORD_EVENT") {
+      recordedClicks.push(msg.data);
       console.log("Recorded click:", msg.data);
     }
+    if(msg.type === "GET_RECORDED_CLICKS"){
+      sendResponse({clicks: recordedClicks})
+    }
+    if(msg.type === "CLEAR_RECORDED_CLICKS"){
+      sendResponse({success:true})
+    }
+    return true;
   });
   
